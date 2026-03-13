bde98fbd5e66175a5ab0a181d2a1aa3b95709661
khanofficial012w-ship-it <khanofficial012w@gmail.com>
2026-03-14 02:13:10 +0500

    Add authentication context and API interceptor for user login/logout functionality

---
+++ src/utils/interceptor.ts
@@ -0,0 +1,80 @@
+import api from "@/api/axios";
+import axios from "axios";
+
+let isRefreshing = false;
+let failedQueue: any[] = [];
+
+const processQueue = (error: any, token: string | null) => {
+  failedQueue.forEach((prom) => {
+    if (error) {
+      prom.reject(error);
+    } else {
+      prom.resolve(token);
+    }
+  });
+
+  failedQueue = [];
+};
+
+export const setupInterceptors = (
+  getToken: () => string | null,
+  setToken: (token: string | null) => void,
+) => {
+  api.interceptors.request.use((config: any) => {
+    const token = getToken();
+
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return config;
+  });
+
+  api.interceptors.response.use(
+    (response) => response,
+    async (error) => {
+      const originalRequest = error.config;
+
+      if (error.response?.status === 401 && !originalRequest._retry) {
+        if (isRefreshing) {
+          return new Promise((resolve, reject) => {
+            failedQueue.push({ resolve, reject });
+          }).then((token: any) => {
+            originalRequest.headers.Authorization = `Bearer ${token}`;
+            return api(originalRequest);
+          });
+        }
+
+        originalRequest._retry = true;
+        isRefreshing = true;
+
+        try {
+          const res = await axios.post(
+            "http://localhost:5000/api/refreshtoken",
+            {},
+            { withCredentials: true },
+          );
+
+          const newToken = res.data.data.accessToken;
+
+          setToken(newToken);
+
+          processQueue(null, newToken);
+
+          originalRequest.headers.Authorization = `Bearer ${newToken}`;
+
+          return api(originalRequest);
+        } catch (err) {
+          processQueue(err, null);
+
+          window.location.href = "/login";
+
+          return Promise.reject(err);
+        } finally {
+          isRefreshing = false;
+        }
+      }
+      return Promise.reject(error);
+    },
+  );
+};
